f985fc997f4b96f7e6e076bd4d6f7cab6151c152
Elias Tazartes <elias.tazartes@gmail.com>
2023-01-12 11:39:37 +0100

    feat: create minimal example for rpc http server

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,3 +1,21 @@
 //! Kakarot RPC module for Ethereum.
 //! It is an adapter layer to interact with Kakarot ZK-EVM.
+use std::net::SocketAddr;
 mod eth_rpc;
+use eth_rpc::KakarotEthRpc;
+use eyre::Result;
+use jsonrpsee::server::ServerBuilder;
+use reth_rpc_api::EthApiServer;
+
+pub async fn run_server() -> Result<SocketAddr> {
+    let server = ServerBuilder::default().build("127.0.0.1:0").await?;
+
+    let addr = server.local_addr()?;
+    let handle = server.start(KakarotEthRpc.into_rpc())?;
+
+    // In this example we don't care about doing shutdown so let's it run forever.
+    // You may use the `ServerHandle` to shut it down or manage it yourself.
+    tokio::spawn(handle.stopped());
+
+    Ok(addr)
+}
--- rpc/src/main.rs
+++ rpc/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    todo!()
+use eyre::Result;
+use futures::future::pending;
+use kakarot_rpc::run_server;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let server_addr = run_server().await?;
+    println!("{server_addr}");
+    pending::<()>().await;
+    Ok(())
 }
